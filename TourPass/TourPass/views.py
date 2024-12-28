c9b10f06444b68b158abbc39e537fc03b404c2da
Sujoy_Sengupta <sujoysengupta313@gmail.com>
2024-12-28 18:20:32 +0530

    Dekh kya kia maine

--- TourPass/TourPass/views.py
+++ TourPass/TourPass/views.py
@@ -13,6 +13,19 @@ def login_view(request):
     '''
     For Sujoy, complete the login view.
     '''
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = login(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'registration/login.html', {'form': form, 'error': 'Invalid username or password. Please enter carefully.'})
+    else:
+        form = AuthenticationForm()
     return render(request,'registration/login.html',{'form':form})
 
 
