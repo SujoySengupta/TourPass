69466b4d4d8bcb1812afbe4ee25d646ca6abe3df
Sujoy Sengupta <sujoysengupta313@gmail.com>
2024-12-29 11:01:12 +0530

    `Added authentication functionality to login view and updated profile page links`

--- TourPass/TourPass/views.py
+++ TourPass/TourPass/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout, authenticate
 from museums.models import *
 from django.contrib.auth.models import User
 
@@ -13,6 +13,17 @@ def login_view(request):
     '''
     For Sujoy, complete the login view.
     '''
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
     return render(request,'registration/login.html',{'form':form})
 
 
